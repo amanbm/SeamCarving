dbdf46ce4236f9c2fa83f1047db36f6ce001009b
Aman Mohammed <amanbm@outlook.com>
2019-12-19 11:43:08 -0800

    got rid of import statement

--- app/src/main/java/com/example/seamcarving/MainActivity.java
+++ app/src/main/java/com/example/seamcarving/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.seamcarving;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Picture;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
