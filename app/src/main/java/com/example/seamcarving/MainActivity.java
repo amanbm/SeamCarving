a79f85316839bffc69c690e3bf51bb40a54ff98e
Aman Mohammed <amanbm@outlook.com>
2019-12-21 14:45:25 -0800

    starting to work on picture capture functionality

--- app/src/main/java/com/example/seamcarving/MainActivity.java
+++ app/src/main/java/com/example/seamcarving/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.seamcarving;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,37 +16,69 @@ public class MainActivity extends AppCompatActivity {
     private int y;
     Button cropHButton;
     Button cropVButton;
-    ImageView image;
+    ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         x = 0;
         y = 0;
+
         cropHButton = findViewById(R.id.cropHButton);
         cropVButton = findViewById(R.id.cropVButton);
-        image = findViewById(R.id.imageView);
-        image.setImageResource(R.drawable.howard);
+        imageView = findViewById(R.id.imageView);
+
+        // Take new picture
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
 
-        // Crop horizontal
+        // TODO: make crop work with seam carving
+        // Scale horizontal
         cropHButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 x++;
-                image.setScaleX(x);
+                imageView.setScaleX(x);
             }
         });
 
-        // Crop vertical
+        // TODO: make crop work with seam carving
+        // scale vertical
         cropVButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 y++;
-                image.setScaleY(y);
+                imageView.setScaleY(y);
             }
         });
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
